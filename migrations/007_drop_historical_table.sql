cf90f18e88944f8ac09b90c4a795d2d1949de7ea
Grisma Bhandari <iamgrisma@gmail.com>
2025-11-06 10:47:24 +0545

    Delete migrations/007_drop_historical_table.sql

--- migrations/007_drop_historical_table.sql
+++
@@ -1,4 +0,0 @@
--- Migration 007: Drop the forex_rates_historical table
--- We're simplifying the database structure to use only the forex_rates table
-
-DROP TABLE IF EXISTS forex_rates_historical;
