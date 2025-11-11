827b1e34de0603cb8bf11712166bb2537f41aa64
Grisma Bhandari <iamgrisma@gmail.com>
2025-11-11 14:37:20 +0545

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
-// 1. Import BrowserRouter instead of HashRouter
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App.tsx';
 import './index.css';
@@ -9,10 +8,9 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
-    {/* 2. Use BrowserRouter here */}
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </QueryClientProvider>
 );
 
@@ -31,11 +29,10 @@ if ('serviceWorker' in navigator) {
           if (newWorker) {
             newWorker.addEventListener('statechange', () => {
               if (newWorker.state === 'installed' && navigator.serviceWorker.controller) {
-                // We don't need a confirm() box here, just reload.
-                // Or, show a custom toast notification.
-                // For now, let's just update.
-                newWorker.postMessage({ type: 'SKIP_WAITING' });
-                // window.location.reload(); // Can be too aggressive
+                if (confirm('New version available! Reload to update?')) {
+                  newWorker.postMessage({ type: 'SKIP_WAITING' });
+                  window.location.reload();
+                }
               }
             });
           }
