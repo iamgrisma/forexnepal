6a90c7b2339b94d559ca9749c19e378b1d7890aa
Grisma Bhandari <iamgrisma@gmail.com>
2025-11-11 19:14:00 +0545

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // <-- FIX 1
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Index from './pages/Index';
 import About from './pages/About';
@@ -13,7 +13,7 @@ import { Toaster } from "@/components/ui/toaster"; // shadcn toaster
 import { Toaster as SonnerToaster } from "@/components/ui/sonner"; // sonner toaster
 import PWAInstallPrompt from './components/PWAInstallPrompt';
 import HistoricalCharts from './pages/HistoricalCharts';
-import CurrencyHistoricalData from './pages/CurrencyHistoricalData'; // <-- FIX 2
+import CurrencyHistoricalData from './pages/CurrencyHistoricalData';
 import ConverterProfitCalculator from './pages/ConverterProfitCalculator';
 import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
@@ -24,7 +24,7 @@ import PostDetail from './pages/PostDetail';
 import PostEditor from './pages/PostEditor';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
-import ChangePassword from './pages/ChangePassword';
+import ChangePassword from './pages/ChangePassword'; // <-- THIS WAS THE FINAL TYPO
 import ApiDocs from './pages/ApiDocs'; 
 
 const queryClient = new QueryClient();
